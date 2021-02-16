d05073f5cfdaba0ecb76ce3d6dbbd8f43a09f015
Aravind Easwar <aeaswar81@gmail.com>
2021-02-16 15:10:30 +0530

    Q4,5 added

--- Assignment1/problem5/assignment1_Q5.jl
+++ Assignment1/problem5/assignment1_Q5.jl
@@ -4,9 +4,8 @@ password=rand(a,8)
 store_count=0
 for _ in 1:1000000
     hacker_gen=rand(a,8)
-    if sum(password.==hacker_gen)>=2
+    if sum(password.==hacker_gen)>=3 #change the matching characters and see the passwords stored
         global store_count+=1
     end
 end
-store_count/=1000000
 print(store_count)
