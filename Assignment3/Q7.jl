5d6251b37b8e4c5dabe2d815e9d02821c6e112d1
Aravind Easwar <aeaswar81@gmail.com>
2021-04-04 23:40:46 +0530

    Q7 minor modification

--- Assignment3/Q7.jl
+++ Assignment3/Q7.jl
@@ -3,6 +3,7 @@ using Distributions
 using Plots
 using StatsPlots
 using StatsBase
+using Statistics
 #funciton for Normal DIstribution 
 function oneSidedTailNormal(x)
     #inverse cdf 
@@ -17,8 +18,8 @@ function oneSidedTailTDist(x)
     x/=100
     return quantile(TDist(10),1-x)
 end
-println("One tail for normal ",oneSidedTailNormal(95))
-println("One tail for TDist",oneSidedTailTDist(95))
+println("One tail at x=95 for normal ",oneSidedTailNormal(95))
+println("One tail at x=95 for TDist",oneSidedTailTDist(95))
 #Plotting 
 plt1=plot(x->x, x->pdf(Normal(0, 1), x), -4, 4, label="Normal")
 plot!(x->x, x->pdf(Normal(0, 1), x), -4, oneSidedTailNormal(95), fill=(0, :orange,0.3),label=false)
